3a5b7fb3b8d99ef1881748c08c56cd5b486e5b88
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-08-31 15:23:38 +0100

    refactor: refactor page to use use layout

--- driver-log-client/pages/index.tsx
+++ driver-log-client/pages/index.tsx
@@ -1,20 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { NextPageWithLayout } from './_app'
+import GeneralLayout from '@/layout/general-layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home: NextPageWithLayout = () => {
   return (
     <>
-      <Head>
-        <title>Driver Log</title>
-        <meta name="description" content="Efficiently Track and Manage Your Driver Logs with Driver Log - Simplify Compliance, Maximize Efficiency!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <h1>Driver Log</h1>
     </>
+  )
+}
+
+export default Home
+
+Home.getLayout = function getLayout(page) {
+  return (
+    <GeneralLayout pageTitle='Home' description='Efficiently Track and Manage Your Driver Logs with Driver Log - Simplify Compliance, Maximize Efficiency!'>
+      {page}
+    </GeneralLayout>
   )
 }
