12e16b4ad55151c5b010c2b729045455e2036042
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-09-02 01:58:40 +0100

    feat: create footer  section

---
+++ driver-log-client/components/footer/footer.tsx
@@ -0,0 +1,15 @@
+import Logo from "../logo/logo";
+import { FooterContainer } from "./footer.styles"
+
+const Footer = () => {
+   return (
+      <FooterContainer>
+         <div>
+            <Logo />
+            <p>©{new Date().getFullYear()} Driver Log. All rights reserved.</p>
+         </div>
+      </FooterContainer>
+   );
+};
+
+export default Footer;
