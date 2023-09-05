c72128e17d469595545c3a210032096fedbb42b1
Isaac Phiri <isaacphiri315@gmail.com>
2023-09-06 01:39:47 +0200

    feat: Create admin routes
    Added routes for admin functionality, including listing all admins, creating an admin, updating admin details, deleting an admin, and getting an admin's profile. These routes are protected and require authentication.

---
+++ backend/routes/api/admin.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { getAdmin, getAdmins, createAdmin, updateAdmin, deleteAdmin, getAdminProfile } = require('../../controllers/adminController');
+const protect = require('../../middleware/authMiddleware');
+
+// routes
+router.get('/', protect, getAdmins);
+router.get('/me', protect, getAdminProfile);
+router.post('/create-admin', createAdmin);
+router.route('/:id').get(protect, getAdmin).put(protect, updateAdmin).delete(protect, deleteAdmin);
+
+module.exports = router;
