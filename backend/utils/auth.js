4f73c03cbe5050eec4557f670d2f7b843c30f202
Isaac Phiri <isaacphiri315@gmail.com>
2023-09-06 01:43:27 +0200

    feat: Add createJWT function for JWT token generation
    Added a new function 'createJWT' for generating JWT tokens with the provided email, userId, and role. The generated token is set as an HTTP-only cookie with security and expiration settings.

---
+++ backend/utils/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const createJWT = (res, email, userId, role) => {
+   const payload = { email, userId, role };
+   const token = jwt.sign(payload, process.env.TOKEN_SECRET, {
+    expiresIn: '30d',
+   });
+
+   res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      samesite: 'strict',
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      });
+};
+
+module.exports = createJWT;
