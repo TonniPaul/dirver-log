0a02d3bbff3d8c76cf6626ff0668ec29f214c59d
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-09-09 15:50:37 +0100

    refactor: add onClick function prop to button

--- driver-log-client/components/button/button.tsx
+++ driver-log-client/components/button/button.tsx
@@ -3,12 +3,13 @@ import { BtnStyle, IBtnStyleSProps } from './button.style';
 
 export interface IButtonProp extends IBtnStyleSProps {
    children: ReactNode;
+   onClick?: () => void;
 }
 
-const Button = ({ primary, children, isBlock }: IButtonProp) => {
+const Button = ({ primary, children, isBlock, onClick }: IButtonProp) => {
 
    return (
-      <BtnStyle primary={primary} isBlock={isBlock}>
+      <BtnStyle primary={primary} isBlock={isBlock} onClick={onClick}>
          {children}
       </BtnStyle>
    );
