99a640c4ac6336871ef79168932497ae5a9ddf38
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-09-10 00:51:18 +0100

    style: improve styling

--- driver-log-client/components/form-input-container/form-input-container.styles.ts
+++ driver-log-client/components/form-input-container/form-input-container.styles.ts
@@ -1,9 +1,12 @@
 import getRemValue from '@/utils/getRemValue';
 import styled from 'styled-components';
+import { IAuthCardStyleProps } from '../cards/auth-card/auth-card.styles';
 
 
 export const FormInputStyles = styled.div`
    position: relative;
+
+   
 
    & > label {
       font-size: ${getRemValue(20)};
@@ -17,25 +20,9 @@ export const FormInputStyles = styled.div`
       position: relative;
       top: -10px;
    }
-
-
-   & > input, & > div > input {
-      border: 1px solid rgb(var(--color-primary), 0.7);
-      outline: none;
-      margin: 1rem 0;
-      width: 100%;
-      padding: 1rem 2rem;
-      border-radius: 25px;
-
-      &::placeholder {
-         font-family: inherit;
-         font-weight: 600;
-         opacity: .6;
-      }
-   }
 `
 
-export const InputFormField = styled.div`
+export const InputFormField = styled.div<IAuthCardStyleProps>`
    position: relative;
 
    & > label {
@@ -54,42 +41,33 @@ export const InputFormField = styled.div`
       position: relative;
       top: -10px;
    }
+`
 
-   & > input {
-      border: 1px solid rgb(var(--color-primary), 0.7);
-      outline: none;
-      margin: 1rem 0;
-      width: 100%;
-      padding: 1rem 2rem;
-      border-radius: 25px;
+export const Input = styled.input<IAuthCardStyleProps>`
+   border: 1px solid rgb(var(--color-primary), 0.7);
+   outline: none;
+   margin: 1rem 0;
+   width: 100%;
+   padding: 1rem 2rem;
+   border-radius: 25px;
 
-      &::placeholder {
+   ${({ isAdmin }) =>
+      isAdmin && `
+      border:1px solid rgb(var(--color-secondary-b));  
+   `
+   }
+
+   &::placeholder {
          font-family: inherit;
          font-weight: 600;
          opacity: .6;
       }
-   }
 `
 
-export const InputFlexContainer = styled.div`
+export const InputFlexContainer = styled.div<IAuthCardStyleProps>`
    width: 100%;
    position: relative;
    display: flex;
-
-   & > input {
-      border: 1px solid rgb(var(--color-primary), 0.7);
-      outline: none;
-      margin: 1rem 0;
-      width: 100%;
-      padding: 1rem 2rem;
-      border-radius: 25px;
-
-      &::placeholder {
-         font-family: inherit;
-         font-weight: 600;
-         opacity: .6;
-      }
-   }
 `
 
 export const TogglePasswordBtn = styled.button`
