80bfad3869902e63848f1438dd34e35bbed91d47
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-09-09 16:33:53 +0100

    refactor: update links

--- driver-log-client/pages/404.tsx
+++ driver-log-client/pages/404.tsx
@@ -1,34 +1,40 @@
 import type { NextPageWithLayout } from './_app';
-import { ErrorPageBoldText, ErrorPageContainer, ErrorPageImageContainer, ErrorPageLinksContainer } from '@/styles/error-page.styles';
+import {
+  ErrorPageBoldText,
+  ErrorPageContainer,
+  ErrorPageImageContainer,
+  ErrorPageLinksContainer,
+} from '@/styles/error-page.styles';
 import Image from 'next/image';
 import LinkButton from '@/components/button/link-button';
 import GeneralLayout from '@/layout/general-layout';
 
-
 const Error404Page: NextPageWithLayout = () => {
-   return (
-      <ErrorPageContainer>
-         <div>
-            <ErrorPageBoldText>Something went wrong.....</ErrorPageBoldText>
-            <p>
-               Please double-check the URL or try refreshing the page. If the issue persists, feel free to contact our support team for assistance
-            </p>
+  return (
+    <ErrorPageContainer>
+      <div>
+        <ErrorPageBoldText>Something went wrong.....</ErrorPageBoldText>
+        <p>
+          Please double-check the URL or try refreshing the page. If the issue
+          persists, feel free to contact our support team for assistance
+        </p>
 
-            <ErrorPageLinksContainer>
-               <LinkButton href={'/contact'}>Contact Us</LinkButton>
-               <LinkButton href={'/'} primary>Go Home</LinkButton>
-            </ErrorPageLinksContainer>
-         </div>
-         <ErrorPageImageContainer>
-            <Image src='/assets/error.svg' alt='error 404 ' fill />
-         </ErrorPageImageContainer>
-
-      </ErrorPageContainer>
-   )
+        <ErrorPageLinksContainer>
+          <LinkButton href={'/contact'}>Contact Us</LinkButton>
+          <LinkButton href={'/'} primary>
+            Go Home
+          </LinkButton>
+        </ErrorPageLinksContainer>
+      </div>
+      <ErrorPageImageContainer>
+        <Image src="/assets/error.svg" alt="error 404 " fill />
+      </ErrorPageImageContainer>
+    </ErrorPageContainer>
+  );
 };
 
 Error404Page.getLayout = (page) => {
-   return <GeneralLayout pageTitle="Page not found">{page}</GeneralLayout>;
+  return <GeneralLayout pageTitle="Page not found">{page}</GeneralLayout>;
 };
 
 export default Error404Page;
