c5e6c2a4631fc4b324fcfedd842d88378c9789eb
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-09-10 01:11:35 +0100

    fix: add rule to temporarily ignore thy of any

--- driver-log-client/components/form-input-container/form-input-container.tsx
+++ driver-log-client/components/form-input-container/form-input-container.tsx
@@ -1,54 +1,66 @@
 import { FC, InputHTMLAttributes, useState } from 'react';
-import { useController, UseControllerProps, ControllerProps } from "react-hook-form"
-import { InputFlexContainer, InputFooterText, TogglePasswordBtn, InputFormField } from "./form-input-container.styles";
-import SvgIcon from "../svg-icon/svg-icon";
+import { useController, UseControllerProps } from 'react-hook-form';
+import {
+  InputFlexContainer,
+  InputFooterText,
+  TogglePasswordBtn,
+  InputFormField,
+  Input,
+} from './form-input-container.styles';
+import SvgIcon from '../svg-icon/svg-icon';
+import { IAuthCardStyleProps } from '../cards/auth-card/auth-card.styles';
 
-interface IFormInputProps extends InputHTMLAttributes<HTMLInputElement> {
-   htmlFor: string;
-   label: string;
-   isPasswordType?: boolean;
-   controller: UseControllerProps<ControllerProps>
+// eslint-disable-line @typescript-eslint/no-explicit-any
+interface IFormInputProps
+  extends IAuthCardStyleProps,
+    InputHTMLAttributes<HTMLInputElement> {
+  htmlFor: string;
+  label: string;
+  isPasswordType?: boolean;
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  controller: UseControllerProps<any>;
 }
 
 const FormInputContainer: FC<IFormInputProps> = ({
-   htmlFor,
-   label,
-   isPasswordType,
-   controller,
-   ...props
+  htmlFor,
+  label,
+  isPasswordType,
+  controller,
+  isAdmin,
+  ...props
 }) => {
-   const { field, fieldState: {error} } = useController(controller)
-   const [showPassword, setShowPassword] = useState(false);
+  const {
+    field,
+    fieldState: { error },
+  } = useController(controller);
+  const [showPassword, setShowPassword] = useState(false);
 
-   return (
-      <InputFormField>
-         <label htmlFor={htmlFor}>{label}</label>
-         <>
-            {isPasswordType ? (
-               <InputFlexContainer>
-                  <input
-                     {...props}
-                     {...{...field}}
-                     type={showPassword ? 'text' : 'password'}
-                  />
-                  <TogglePasswordBtn
-                     type="button"
-                     onClick={() => setShowPassword(!showPassword)}
-                  >
-                     <SvgIcon name={!showPassword ? 'eye-open' : 'eye-close'} />
-                  </TogglePasswordBtn>
-               </InputFlexContainer>
-            ) : (
-               <input {...props} {...{...field}}  />
-            )}
-         </>
-         <>
-            {error && 
-               <InputFooterText>{error.message}</InputFooterText>
-            }
-         </>
-      </InputFormField>
-   );
-}
+  return (
+    <InputFormField>
+      <label htmlFor={htmlFor}>{label}</label>
+      <>
+        {isPasswordType ? (
+          <InputFlexContainer>
+            <Input
+              {...props}
+              {...{ ...field }}
+              type={showPassword ? 'text' : 'password'}
+              isAdmin={isAdmin}
+            />
+            <TogglePasswordBtn
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <SvgIcon name={!showPassword ? 'eye-open' : 'eye-close'} />
+            </TogglePasswordBtn>
+          </InputFlexContainer>
+        ) : (
+          <Input isAdmin={isAdmin} {...props} {...{ ...field }} />
+        )}
+      </>
+      <>{error && <InputFooterText>{error.message}</InputFooterText>}</>
+    </InputFormField>
+  );
+};
 
 export default FormInputContainer;
