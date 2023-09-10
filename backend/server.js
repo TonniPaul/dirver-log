072ee25811f78a32fc254cc8b0e3123bbe4103b9
Isaac Phiri <isaacphiri315@gmail.com>
2023-09-10 13:08:30 +0200

    feat: updated server

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Error handling middleware
 app.use(notFound);
