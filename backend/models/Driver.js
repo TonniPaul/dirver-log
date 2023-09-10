6167650f25ce0e8a0c2871954f0a1703c15c61b5
Isaac Phiri <isaacphiri315@gmail.com>
2023-09-10 14:14:32 +0200

    feat: updated Driver model to conatain the admin object id that created it..

--- backend/models/Driver.js
+++ backend/models/Driver.js
@@ -47,8 +47,14 @@ const DriverSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
-}, { timestamps: true
-});
+	admin: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Admin',
+		required: false,
+	},
+},
+{ timestamps: true }
+);
 
 const Driver = mongoose.model('Driver', DriverSchema);
 module.exports = Driver;
