20e7dd333ccfcb824af284490ef190b16087f629
ARIYO-ADEOYE PAUL OLUWATONI <97785259+TonniPaul@users.noreply.github.com>
2023-09-12 02:45:11 +0100

    Revert "Create dashboard"

--- driver-log-client/components/logo/logo.styles.ts
+++ driver-log-client/components/logo/logo.styles.ts
@@ -2,29 +2,19 @@ import getRemValue from '@/utils/getRemValue';
 import Link from 'next/link';
 import styled from 'styled-components';
 
-export interface ILogoStyleProps {
-  isPrimary?: boolean;
-}
+export const LogoContainer = styled(Link)`
+   display: flex;
+   align-items: center;
+   z-index: 10;
 
-export const LogoContainer = styled(Link)<ILogoStyleProps>`
-  display: flex;
-  align-items: center;
-  z-index: 10;
+   & > svg {
+      fill: currentColor;
+      width: ${getRemValue(35)};
+      height: ${getRemValue(35)};
+   }
 
-  ${({ isPrimary }) =>
-    isPrimary &&
-    `
-    color: rgb(var(--color-primary));
-  `}
-
-  & > svg {
-    fill: currentColor;
-    width: ${getRemValue(35)};
-    height: ${getRemValue(35)};
-  }
-
-  & > p {
-    font-size: ${getRemValue(20)};
-    font-weight: 700;
-  }
+   & > p {
+      font-size: ${getRemValue(20)};
+      font-weight: 700;
+   }
 `;
--- driver-log-client/components/logo/logo.tsx
+++ driver-log-client/components/logo/logo.tsx
@@ -1,13 +1,14 @@
-import { ILogoStyleProps, LogoContainer } from './logo.styles';
-import SvgIcon from '../svg-icon/svg-icon';
+import Image from "next/image";
+import { LogoContainer } from "./logo.styles";
+import SvgIcon from "../svg-icon/svg-icon";
 
-const Logo = ({ isPrimary }: ILogoStyleProps) => {
-  return (
-    <LogoContainer href="/" isPrimary={isPrimary}>
-      <SvgIcon name="driver" />
-      <p>Driver Log</p>
-    </LogoContainer>
-  );
+const Logo = () => {
+   return (
+      <LogoContainer href='/'>
+         <SvgIcon name='driver' />
+         <p>Driver Log</p>
+      </LogoContainer>
+   );
 };
 
 export default Logo;
