26351972aaf0c0132cd1b1645639acd4ad4e810f
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-09-12 02:12:38 +0100

    feat: create dashbord

--- driver-log-client/components/logo/logo.tsx
+++ driver-log-client/components/logo/logo.tsx
@@ -1,9 +1,9 @@
-import { LogoContainer } from './logo.styles';
+import { ILogoStyleProps, LogoContainer } from './logo.styles';
 import SvgIcon from '../svg-icon/svg-icon';
 
-const Logo = () => {
+const Logo = ({ isPrimary }: ILogoStyleProps) => {
   return (
-    <LogoContainer href="/">
+    <LogoContainer href="/" isPrimary={isPrimary}>
       <SvgIcon name="driver" />
       <p>Driver Log</p>
     </LogoContainer>
