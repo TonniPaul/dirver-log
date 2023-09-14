04a461bc15114353ca45951e24728496b97cd908
Isaac Phiri <isaacphiri315@gmail.com>
2023-09-14 05:17:23 +0200

    feat: changed email notification to use gmail api from sendgrid api

--- backend/utils/emailNotifications.js
+++ backend/utils/emailNotifications.js
@@ -1,20 +1,57 @@
-const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer');
+const Mailgen = require('mailgen');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const config = {
+    service : 'gmail',
+    auth : {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.GMAIL_PASSWORD
+    }
+};
 
-const emailNotification = async (options) => {
-    try {
-        const msg = {
-            to: options.email,
-            from: options.from,
-            subject: options.subject,
-            text: options.message,
-            html: options.html
+const transporter = nodemailer.createTransport(config);
+
+const MailGenerator = new Mailgen({
+    theme: 'default',
+    product : {
+        name: "Mailgen",
+        link : 'https://mailgen.js/'
+    }
+});
+
+const emailNotification = async (userEmail, logEntry, req) => {
+    const response = {
+        body: {
+            name : "DriverLog",
+            intro: "New Log Entry Created!",
+            table : {
+                data : [
+                    {
+                      item : `<p>New log entry created by ${req.user.firstName + ' ' + req.user.lastName} : </p> <p>on: ${logEntry.logDate} </p> <p>Distance: ${logEntry.trip.distance} </p> <p>Vehicle: ${logEntry.vehicle.licensePlate} </p> <p>Purpose: ${logEntry.trip.purpose} </p> <p>Remarks: ${logEntry.comments}</p>`,
+                      description: "Log Entry",
+                    }
+                ]
+            },
+            outro: `Log entry created by ${req.user.firstName + ' ' + req.user.lastName}`
         }
-        await sgMail.send(msg)
-    } catch (error) {
-        console.log(error)
+    };
+
+    const mail = MailGenerator.generate(response);
+
+    const message = {
+        from : process.env.SENDER_EMAIL,
+        to : userEmail,
+        subject: "Driver Log Entry",
+        html: mail
+    };
+
+    try {
+        await transporter.sendMail(message);
+        console.log(`Email sent to ${userEmail}`);
+    } catch(err) {
+        console.error(`Error sending email to ${userEmail}:`, err);
+        throw new Error('Internal server error');
     }
 }
 
-module.exports = emailNotification;
+module.exports = { emailNotification };
