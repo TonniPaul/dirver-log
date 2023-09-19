35413dc18416b588f51a4506df9de01020b3c606
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-09-19 20:48:25 +0100

    style: improve styling

--- driver-log-client/styles/dashboard-page.styles.ts
+++ driver-log-client/styles/dashboard-page.styles.ts
@@ -10,13 +10,6 @@ export const DashboardContainer = styled.div`
     justify-content: space-between;
     display: flex;
   }
-  & > * {
-    padding: ${getRemValue(15)};
-
-    @media screen and (min-width: 62.5em) {
-      padding: ${getRemValue(40)} 2rem;
-    }
-  }
 `;
 export const SidebarContainer = styled.div`
   background-color: rgb(var(--color-white));
@@ -25,14 +18,17 @@ export const SidebarContainer = styled.div`
   align-items: center;
   width: 100%;
   height: max-content;
+  padding: ${getRemValue(15)};
   padding-right: 1rem;
   padding-left: 1rem;
   text-align: center;
+  box-shadow: var(--box-shadow);
 
   @media screen and (min-width: 62.5em) {
     display: block;
     width: ${getRemValue(230)};
     height: 100vh;
+    padding: ${getRemValue(30)} 1rem;
   }
 `;
 
@@ -48,7 +44,6 @@ export const SidebarItems = styled.ul`
   background-color: rgb(var(--color-white));
   box-shadow: 0px -1px 0px rgb(var(--color-primary), 0.2);
   padding-top: 1rem;
-  padding-left: 1.5rem;
 
   @media screen and (min-width: 62.5em) {
     position: relative;
@@ -59,7 +54,7 @@ export const SidebarItems = styled.ul`
   }
 `;
 
-export const SidebarMobileMenu = styled.button`
+export const SidebarMobileMenu = styled.div`
   & > svg {
     fill: green !important;
     color: rgb(var(--color-primary));
@@ -72,24 +67,43 @@ export const SidebarMobileMenu = styled.button`
   }
 `;
 
-export const LogoutButton = styled.button`
-  background-color: rgb(var(--color-error), 0.09);
-  color: rgb(var(--color-error));
-  width: max-content;
-  margin: 0 auto;
-  border: 1px solid rgb(var(--color-error));
-  border-radius: 5px;
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-  padding: 1rem;
+export const LogoutButton = styled.div<{ hideOnMobile?: boolean }>`
+  & > * {
+    background-color: rgb(var(--color-error), 0.09);
+    color: rgb(var(--color-error));
+    width: max-content;
+    margin: 0 auto;
+    border: 1px solid rgb(var(--color-error));
+    border-radius: 5px;
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    padding: 1rem;
 
-  & > svg {
-    width: 20px;
-    height: 20px;
+    & > svg {
+      width: 20px;
+      height: 20px;
+    }
+
+    @media screen and (max-width: 62.5em) {
+      background: none !important;
+      border: none;
+
+      ${({ hideOnMobile }) =>
+        hideOnMobile &&
+        `
+         display: none;
+      `}
+    }
   }
+`;
 
-  @media screen and (max-width: 62.5em) {
+export const DashboardMobileSignOutMenu = styled.div`
+  padding: 1rem 2rem;
+  background-color: rgb(var(--color-white));
+  border: 1px solid rgb(var(--color-primary));
+
+  @media screen and (min-width: 62.5em) {
     display: none;
   }
 `;
@@ -107,4 +121,23 @@ export const RightSidebar = styled.div`
   background-color: rgb(var(--color-white));
   height: 100vh;
   width: ${getRemValue(350)};
+`;
+
+export const DashboardContent = styled.div`
+  flex: 1;
+
+  & > div {
+    padding: 2rem;
+  }
+`;
+
+export const DashboardNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  align-self: flex-start;
+  flex: 1;
+  background-color: rgb(var(--color-white));
+  box-shadow: var(--box-shadow);
 `;
