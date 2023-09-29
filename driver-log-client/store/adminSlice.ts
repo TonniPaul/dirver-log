c668c4e25848d33b8ad65be443d42c97ded096b6
Isaac Phiri <isaacphiri315@gmail.com>
2023-09-30 01:43:53 +0200

    🦄 refactor(Cookie Token): Redundant Token
    Edit and remove code to fetch token from the response body and store in the cookies as it is already being done in the backend in (utils/auth.js)

--- driver-log-client/store/adminSlice.ts
+++ driver-log-client/store/adminSlice.ts
@@ -10,16 +10,9 @@ export interface ISessionSliceType {
 export const adminSlice: StateCreator<ISessionSliceType> = (set) => ({
   admin: null,
   setAdmin: (data) => {
-    // Set the admin data in state
     set({ admin: data });
-    // Assuming 'data.token' is the token received from the backend
-    const token = data.token;
-    // Set the token as a cookie
-    document.cookie = `adminToken=${token}; expires=${new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString()}; path=/`;
   },
   clearAdmin: () => {
     set({ admin: null });
-    // Clear the token cookie
-    document.cookie = 'adminToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
   },
 });
