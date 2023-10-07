21cc7929d9fbfe9dd865f584d93e2c87fab8ba76
Isaac Phiri <isaacphiri315@gmail.com>
2023-10-07 15:44:35 +0200

    update auth.js

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -8,10 +8,11 @@ const createJWT = (res, email, userId, role) => {
 
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    samesite: 'strict',
+    secure: process.env.NODE_ENV === 'development' ? true : false,
+    sameSite: 'none',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
+  return token;
 };
 
 module.exports = createJWT;
