4039c135badb0ecb7b44a85a7ca0f02f96a16728
Isaac Phiri <isaacphiri315@gmail.com>
2023-10-07 15:44:30 +0200

    update TripLog.js

--- backend/models/TripLog.js
+++ backend/models/TripLog.js
@@ -2,79 +2,74 @@ const mongoose = require('mongoose');
 
 const TripLogSchema = new mongoose.Schema(
   {
-    origin: {
+    startLng: { 
+      type: Number
+    },
+    startLat: { 
+      type: Number
+    },
+    originAddress: { 
+      type: String
+    },
+    destinationAddress: { 
+      type: String
+    },
+    purpose: { 
+      type: String
+    },
+    driver: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Driver'
+    },
+    vehicle: { 
       type: String,
     },
-    destination: {
+    endLng: { 
+      type: Number
+    },
+    endLat: { 
+      type: Number
+    },
+    comments: { 
+      type: String 
+    },
+    distance: { 
       type: String,
+      get: v => `${Math.floor(v)}(Km)`,
     },
-    startMileage: {
-      type: Number,
-    },
-    endMileage: {
-      type: Number,
-    },
-    purpose: {
-      type: String,
-    },
-    distance: {
-      type: Number,
-    },
-    totalMileage: {
-      type: Number,
-    },
-    driver: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Driver',
-    },
-    logDate: {
-      type: Date,
-      default: Date.now,
-    },
-    vehicle: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Vehicle',
-    },
-    comments: {
-      type: String,
-    },
-  },
-  { timestamps: true }
+},
+{
+  timestamps: true
+}
 );
 
-// Calculate distance and total mileage before saving the trip
-TripLogSchema.pre('save', function (next) {
-  if (this.destination && this.endMileage) {
-    this.distance = calculateDistance(this.origin, this.destination);
-    this.totalMileage = this.endMileage - this.startMileage;
-  }
-  next();
+// Calculate and set the distance before saving the document
+TripLogSchema.pre('save', function(next) {
+    const start = {
+        lat: this.startLat,
+        lon: this.startLng
+    };
+    const end = {
+        lat: this.endLat,
+        lon: this.endLng
+    };
+    
+    // Calculate distance using Haversine formula
+    const radius = 6371; // Earth's radius in kilometers
+    const dLat = (end.lat - start.lat) * Math.PI / 180;
+    const dLon = (end.lon - start.lon) * Math.PI / 180;
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(start.lat * Math.PI / 180) * Math.cos(end.lat * Math.PI / 180) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = radius * c;
+
+    // Round down the distance and convert it to an integer
+    this.distance = Math.floor(distance);
+    
+    next();
 });
 
-//using the Haversine formula to calculate the distance between
-//two sets of latitude and longitude coordinates
-function calculateDistance(origin, destination) {
-  const [lat1, lon1] = origin.split(',').map(parseFloat);
-  const [lat2, lon2] = destination.split(',').map(parseFloat);
+const Triplog = mongoose.model('Triplog', TripLogSchema);
 
-  const earthRadius = 6371; // Radius of the Earth in kilometers
-
-  // Convert latitude and longitude to radians
-  const dLat = ((lat2 - lat1) * Math.PI) / 180;
-  const dLon = ((lon2 - lon1) * Math.PI) / 180;
-
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = earthRadius * c;
-
-  return distance;
-}
-
-const TripLog = mongoose.model('TripLog', TripLogSchema);
-module.exports = TripLog;
+module.exports = Triplog;
